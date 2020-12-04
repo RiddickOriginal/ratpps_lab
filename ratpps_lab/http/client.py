617dad9623d86e446b0c7e6d741e9a2764d75e1d
Vyacheslav <riddickoriginal@gmail.com>
2020-12-05 00:33:03 +0400

    Add files via upload

--- ratpps_lab/http/client.py
+++ ratpps_lab/http/client.py
@@ -1,32 +1,32 @@
-import http.client as cl
+import requests
 
-class HttpClient:
-    
-    def __init__(self, host, port="80"):
-        self.conn = cl.HTTPConnection("{}:{}".format(host, port))
-        self.res = ""
+class TolltechClient:
 
-    def GET(self, addr="/", headers=-1):
-        self.res = self.__request("GET", addr, headers)
+    def __init__(self):
+        self.host = "https://tolltech.ru/study/"
+        self.r = ""
 
-    def POST(self, addr="/", headers=-1):
-        self.res = self.__request("POST", addr, headers)
+    def ping(self):
+        self.r = requests.get(self.host+"Ping")
+        return True if self.r.status_code == 200 else False
 
-    def __request(self, req_type, req_addr, headers):
-        if headers == -1:
-            self.conn.request(req_type, req_addr)
-        else:
-            self.conn.request(req_type, req_addr, headers)
-        res = self.conn.getresponse()
-        return res
+    def find(self, key: str):
+        self.r = requests.get(self.host+"Find", params={'key': key})
+        return self.r.json() if self.r.text != 'null' else None
 
-    def read_last_response(self, chunks=-1):
-        if chunks == -1:
-            return self.res.read()
-        return self.res.read(chunks)
+    def select(self, keys: list):
+        params = "&".join(['keys={}'.format(key) for key in keys])
+        self.r = requests.get(self.host+"Select", params=params)
+        return self.r.json()
 
-    def conn_close():
-        self.conn.close()
-        self.res = ""
-        
-        
+    def create(self, key: str, value: str):
+        self.r = requests.post(self.host+"Create", json={'Key': key, 'Value': value})
+        return self.r.status_code, self.r.reason
+
+    def create_all(self, params={}):
+        self.r = requests.post(self.host+"CreateAll", json=params)
+        return self.r.status_code, self.r.reason
+
+    def update(self, key: str, new_value: str):
+        self.r = requests.post(self.host+"Update?key={}&value={}".format(key, new_value))
+        return self.r.status_code, self.r.reason
--- ratpps_lab/http/tests.py
+++ ratpps_lab/http/tests.py
@@ -1,21 +1,52 @@
 import unittest
-from client import HttpClient
+from client import TolltechClient
+import random as rnd
 
 class TestClient(unittest.TestCase):
 
-    test_data = b'<!DOCTYPE html>\n<HTML>\n<HEAD>\n <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">\n '
+    def test_create(self):
+        client = TolltechClient()
+        status = client.create(rnd.randint(10,100), "testvalue1")
+        self.assertTrue(status == (200, 'OK'))
 
-    def test_post(self):
-        conn = HttpClient("www.axion.com", "80")
-        conn.GET()
-        conn_data = conn.read_last_response(100)
-        self.assertEqual(conn_data, self.test_data)
+    def test_find_random_key(self):
+        client = TolltechClient()
+        key_value = client.find("randomkey")
+        self.assertFalse(key_value)
 
-    def test_get(self):
-        conn = HttpClient("www.axion.com", "80")
-        conn.POST()
-        conn_data = conn.read_last_response(100)
-        self.assertEqual(conn_data, self.test_data)
+    def test_find_real_key(self):
+        client = TolltechClient()
+        client.create("key1", "testvalue1")
+        key_value = client.find("testkey1")
+        self.assertTrue(key_value)
+
+    def test_update_key(self):
+        client = TolltechClient()
+        key = str(rnd.randint(10,100))+"upd"
+        client.create(key, "testvalue")
+        client.update(key, "valuetest")       
+        find_value = client.find(key)["Value"]
+        self.assertEqual(find_value, "valuetest")
+
+    def test_select(self):
+        client = TolltechClient()    
+        key1 = str(rnd.randint(10,100))+"sel"
+        key2 = str(rnd.randint(10,100))+"sel"
+        client.create(key1, "testvalue1")
+        client.create(key2, "testvalue2")
+        kv_dict = client.select([key1, key2])
+        self.assertEqual([{'Key': key1, 'Value': 'testvalue1'}, {'Key': key2, 'Value': 'testvalue2'}], kv_dict)
+
+    def test_create_all(self):
+        client = TolltechClient()
+        key1 = str(rnd.randint(10,100))+"sel"
+        key2 = str(rnd.randint(10,100))+"sel"
+        kv_dict = [{'Key': key1, 'Value': 'v1'}, {'Key': key2, 'Value': 'v2'}]
+        client.create_all(kv_dict)
+        test_kv_dict = client.select([key1, key2])
+        self.assertEqual(kv_dict, test_kv_dict)
+
+        
 
 
 if __name__ == '__main__':
