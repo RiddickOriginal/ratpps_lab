fd31515436539c4e8c90efa6a23451832fdb5a30
Vyacheslav <riddickoriginal@gmail.com>
2020-12-06 15:06:45 +0400

    Delete main.py

--- ratpps_lab/http/main.py
+++
@@ -1,5 +0,0 @@
-from client import TolltechClient
-
-client = TolltechClient()
-client.create_all([{'Key': 'k1', 'Value': 'v1'}, {'Key': 'k2', 'Value': 'v2'}, {'Key': 'k3', 'Value': 'v3'}])
-print(client.select(['k1', 'k2', 'k3']))
